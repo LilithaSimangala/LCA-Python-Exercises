94c1ee156d555c4c1d102b0cfb5b305c32f7c51a
Lilitha Simangala <lilithaphem@gmail.com>
2026-04-21 20:11:25 +0200

    Add Exercise 01 - Introduction and Variables

---
+++ week3_ex01_variables.py
@@ -0,0 +1,17 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+print("Hello", name, "you are", age, "years old.")
+
+length = int(input("Enter rectangle length: "))
+width = int(input("Enter rectangle width: "))
+
+area = length * width
+
+print("The area of the rectangle is:", area)
+
+celsius = float(input("Enter temperature in Celsius: "))
+
+fahrenheit = (celsius * 9/5) + 32
+
+print("Temperature in Fahrenheit is:", round(fahrenheit, 2))
